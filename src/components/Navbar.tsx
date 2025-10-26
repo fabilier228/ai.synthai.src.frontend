16f469d195ed0780918b7cb98e7d4ae07ff6c281
Sylwia <s.kruszynska.139@studms.ug.edu.pl>
2025-10-26 21:30:16 +0100

    refactor: change style to tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,26 +6,25 @@ import {
   LocalLibrary,
   Person,
 } from "@mui/icons-material";
-import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <HomeFilled />
+    <nav className="fixed left-0 bottom-0 w-screen bg-surface border-t border-outline py-2 z-[100]">
+      <ul className="flex flex-row justify-between items-center w-full m-0 p-0 list-none">
+        <li className="flex-1 flex justify-center items-center">
+          <HomeFilled className="text-[2.2rem] text-primary_muted transition-colors duration-200" />
         </li>
-        <li>
-          <ModelTraining />
+        <li className="flex-1 flex justify-center items-center">
+          <ModelTraining className="text-[2.2rem] text-primary_muted transition-colors duration-200" />
         </li>
-        <li>
-          <AddCircleOutline />
+        <li className="flex-1 flex justify-center items-center">
+          <AddCircleOutline className="text-[2.2rem] text-primary_muted transition-colors duration-200" />
         </li>
-        <li>
-          <LocalLibrary />
+        <li className="flex-1 flex justify-center items-center">
+          <LocalLibrary className="text-[2.2rem] text-primary_muted transition-colors duration-200" />
         </li>
-        <li>
-          <Person />
+        <li className="flex-1 flex justify-center items-center">
+          <Person className="text-[2.2rem] text-primary_muted transition-colors duration-200" />
         </li>
       </ul>
     </nav>
