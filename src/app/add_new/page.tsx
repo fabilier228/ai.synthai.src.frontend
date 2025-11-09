e4b6ff9e98601164b42b199ed6fc547c28765573
Martyna <m.kolaska04@gmail.com>
2025-11-09 19:09:14 +0100

    remove unnecessary min-height

--- src/app/add_new/page.tsx
+++ src/app/add_new/page.tsx
@@ -1,6 +1,6 @@
 export default function AddNewPage() {
   return (
-    <div className="min-h-screen flex flex-col justify-between mt-20 md:mt-8">
+    <div className=" flex flex-col justify-between mt-20 md:mt-8">
       <h1 className="text-3xl font-bold text-center mt-10">Add New Page</h1>
     </div>
   );
